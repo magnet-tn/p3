f4ca85dcfe873a90aef8c268ba39183a36246eea
Thomas Narro <Thomas.Narro@gmail.com>
2016-11-28 19:29:20 -0500

    edited welcome page for more clarity

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,32 @@
 
 @section('title', 'Developers BF')
 
+
 @section('headline')
     <h1>Developers BF</h1>
+
+        <p>Welcome. This is <em>Developer’s BF </em>.
+            We have some tools to help you quickly create filler content
+            and users to test your web applications.</p>
+
+
+    <h4>Ipsum Lorem<span class="sscript">*</span></sup> Generator</h4>
+
+    <p>Uses Joshtronic API to generate lorem filler text in
+        denominations of paragraphs, sentences or words.</p>
+
+    <h4>Random User Generator</h4>
+
+    <p>Uses the RandomUser API to generate users with
+        various properties, including name, gender, address, date of birth,
+        username, password and a brief bio using lorem ipsum.</p>
 @endsection
 
 @section('content')
-    <p>Welcome. This is The Developers Best Friend. </br>
-    We have some tools to help you quickly create filler content and users to test your web applications.</p>
+
+
+    <span class="sscript">*</span><span class="footnote">Lorem ipsum is actually
+        scrambled Latin that used to mock up or stage a page of for considering
+        the graphical design, without getting distracted by actual
+        meaningful content.</span>
 @endsection
